7fb65652801335007d075c556825d7def4a1c73d
thangnx183 <nxthang881972@gmail.com>
2022-08-24 08:16:39 +0000

    add index of damage in final result

--- video_process/damage/scratch.py
+++ video_process/damage/scratch.py
@@ -30,6 +30,7 @@ class RemoveScratchByYolo(FilterDecorator):
                     overlap=True
                     break
             if not overlap:
+                pred_json['scratch']['labels'].pop(i)
                 pred_json["scratch"]["masks"].pop(i)
                 pred_json["scratch"]["bboxes"].pop(i)
                 pred_json["scratch"]["scores"].pop(i)
@@ -59,8 +60,11 @@ class Scratch(DamageDecorator):
                 continue
             damage_res, pred_damage_list,score_list,bboxes_list=pred_json[dam]["labels"], pred_json[dam]["masks"],pred_json[dam]["scores"],pred_json[dam]["bboxes"]
             
-            ndamage_per_carpart=0
+
             for kn in range(len(pred_damage_list)):
+                ## TODO : add filter related to lower confident score here
+                #
+                # 
 
                 pred_damage = pred_damage_list[kn]
                 if cv2.countNonZero(pred_damage)==0:
@@ -80,19 +84,14 @@ class Scratch(DamageDecorator):
                     if damage_pixel / min(cv2.countNonZero(pred_damage),cv2.countNonZero(carpart_mask)) < 0.05:
                         continue
                     # if damage_pixel / max(cv2.countNonZero(pred_damage),cv2.countNonZero(carpart_mask)) > 0.75:
-                    #     dam='missing'
-                    ndamage_per_carpart +=1
-                    max_score = max(max_score, float(score_list[kn]))
+                    #     dam='missing
 
-        
-            damage_score = (dam, max_score,ndamage_per_carpart)
-
-            if max_score > 0:
-                if cat not in final_output.keys():
-                    final_output[cat] = []
-                    final_output[cat].append(damage_score)
-                else:
-                    final_output[cat].append(damage_score)
+                    damage_score = (dam, score_list[kn],kn)
+                    if cat not in final_output.keys():
+                        final_output[cat] = []
+                        final_output[cat].append(damage_score)
+                    else:
+                        final_output[cat].append(damage_score)
 
         return self.component.get_damage2carpart(image,pred_json,final_output)
 
