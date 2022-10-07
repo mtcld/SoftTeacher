620509b933ff716ca23eba4b6681230316934e2f
thangnx183 <nxthang881972@gmail.com>
2022-10-07 08:38:18 +0000

    first setup for new pipeline

--- video_process/Hungarian_matching.py
+++ video_process/Hungarian_matching.py
@@ -1,3 +1,4 @@
+from distutils.log import info
 from turtle import Turtle
 from xmlrpc.client import FastParser
 from scipy.optimize import linear_sum_assignment
@@ -63,7 +64,7 @@ class Hungarian():
         self.count_frame = 0
         self.cache = {}
         self.history = {}
-        self.threshold = 6
+        self.threshold = 4
 
     def normalize(self,box):
         xyxy = np.array([box[0],box[2]]).reshape(-1)
@@ -73,6 +74,9 @@ class Hungarian():
     def get_label_carpart(self,label):
         return label[:label.find('+')]
     
+    def reset_count_frame(self):
+        self.count_frame = 0
+
     def bipartite_matching(self,track_info, detect_info,pred_json,view_dict,view_id,file_name):
         # base on tracking of previous frame + detect of current frame -> relabel current detection -> output new roi list and pred_json
         self.count_frame += 1
@@ -133,7 +137,8 @@ class Hungarian():
                 if cache_label not in self.cache.keys():    
                     self.cache[cache_label] = [self.count_frame,1]
                 else:
-                    if self.count_frame - self.cache[cache_label][0] == 1:
+                    print('debug hungarian : ',self.count_frame, self.cache[cache_label][0])
+                    if self.count_frame - self.cache[cache_label][0] == 1 :
                         # self.cache[cache_label]=[self.count_frame,self.cache[cache_label][1]+1]
                         self.cache[cache_label][0] = self.count_frame
                         self.cache[cache_label].insert(1,self.cache[cache_label][1]+1)
@@ -142,8 +147,8 @@ class Hungarian():
                         self.cache[cache_label][0] = self.count_frame
                         self.cache[cache_label].insert(1,1)
 
-                # with open('video_tool/cache-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
-                #     json.dump(self.cache, f, ensure_ascii=False, indent=4)
+                with open('video_tool/cache-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
+                    json.dump(self.cache, f, ensure_ascii=False, indent=4)
                 
                 if self.cache[cache_label][1] >= self.threshold :
                     # modify view_dict to reverse change
@@ -179,8 +184,8 @@ class Hungarian():
                 else:
                     self.history[view_id].append(record)
 
-                # with open('video_tool/history-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
-                #     json.dump(self.history, f, ensure_ascii=False, indent=4)
+                with open('video_tool/history-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
+                    json.dump(self.history, f, ensure_ascii=False, indent=4)
 
                 check_relabel_flag = True
 
@@ -190,6 +195,157 @@ class Hungarian():
 
         return detect_info, pred_json, check_relabel_flag, view_dict
     
+    def bipartite_matching_v2(self,track_info, detect_info,pred_json,view_dict,view_id,frame_id,file_name):
+        # base on tracking of previous frame + detect of current frame -> relabel current detection -> output new roi list and pred_json
+        self.count_frame += 1
+
+        track_labels = []
+        track_boxes = []
+        track_ind = []
+
+        for label,box,idx in track_info:
+            track_labels.append(label)
+            track_boxes.append(self.normalize(box))
+            track_ind.append(idx)
+        
+        detect_labels = []
+        detect_boxes = []
+        detect_ind = []
+
+        for label, box, idx in detect_info:
+            detect_labels.append(label)
+            detect_boxes.append(self.normalize(box))
+            detect_ind.append(idx)
+        
+        if len(detect_boxes) == 0 or len(track_boxes) == 0:
+            return detect_info, pred_json, False, view_dict
+        
+        cost_global = []
+        
+        for idx,dbox in enumerate(detect_boxes):
+            row_cost = []
+            for idy,tbox in enumerate(track_boxes):
+                row_cost.append(loss(detect_labels[idx],dbox,track_labels[idy],tbox))
+            cost_global.append(row_cost)
+        
+        cost_global = np.array(cost_global)
+        cost_global = np.pad(cost_global,[(0,int(cost_global.shape[0]<cost_global.shape[1])*abs(cost_global.shape[0]-cost_global.shape[1])),
+                                        (0,int(cost_global.shape[0]>cost_global.shape[1])*abs(cost_global.shape[0]-cost_global.shape[1]))],
+                                        'constant',constant_values=(10,))
+
+        row_ind,col_ind = linear_sum_assignment(cost_global)
+
+        check_relabel_flag = False
+
+        # if self.count_frame > 300 and self.count_frame < 420: 
+        #     check_relabel_flag = True
+
+        for r,c in zip(row_ind,col_ind):
+            if r >= len(detect_labels) or c >= len(track_labels):
+                continue
+            
+            if cost_global[r,c] < 0.5:
+                if self.get_label_carpart(detect_info[r][0]) != self.get_label_carpart(track_labels[c]) and cost_global[r,c] > 0.38 :
+                    continue
+
+                if detect_labels[r] == track_labels[c]:
+                    continue
+                
+                cache_label = detect_labels[r]+'/'+track_labels[c]
+                if cache_label not in self.cache.keys():    
+                    self.cache[cache_label] = [self.count_frame,1,0]
+                else:
+                    print('debug hungarian : ',self.count_frame, self.cache[cache_label][0])
+                    count_check = self.count_frame - self.cache[cache_label][0]
+                    if count_check <= 1 :
+                        self.cache[cache_label]=[self.count_frame,self.cache[cache_label][1]+1,self.cache[cache_label][2]+count_check]
+                        # self.cache[cache_label][0] = self.count_frame
+                        # self.cache[cache_label].insert(1,self.cache[cache_label][1]+1)
+                    else:
+                        self.cache[cache_label]=[self.count_frame,1,0]
+                        # self.cache[cache_label][0] = self.count_frame
+                        # self.cache[cache_label].insert(1,1)
+
+                with open('video_tool/cache-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
+                    json.dump(self.cache, f, ensure_ascii=False, indent=4)
+                
+                if self.cache[cache_label][1] >= self.threshold :
+                    # modify view_dict to reverse change
+                    # try to reserver all the wrong label cause by wrong tracking infomation
+                    # frame_ids = list(self.history.keys())
+                    for i in range(1,self.threshold):
+                        curr_id = view_id - i
+                        print('trying to reverse .........',curr_id,view_dict[curr_id]['frame_id'])
+
+                        if self.cache[cache_label][2] <= 1 :
+                            for rc in self.history[9]:
+
+                                if view_dict[curr_id]['frame_id'] != rc['frame_id'] :
+                                    continue
+                                    
+                                print('debug len view dict : ',len(view_dict))
+                                print('index : ',rc['index_detect'])
+                                print('len labels ', len(view_dict[curr_id]['result'][0]['carpart']['labels']))
+
+                                if view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']] == track_labels[c]:
+                                    print('done 1 reverse ')
+                                    # print('record :',rc['detect'],rc['track'])
+                                    # print('cur label : ',view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']])
+                                    view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']] = detect_labels[r]
+                                    origin_id = rc['index_track']
+                        else:
+                            for rc in self.history[curr_id]:
+
+                                if view_dict[curr_id]['frame_id'] != rc['frame_id'] :
+                                    continue
+                                    
+                                print('debug len view dict : ',len(view_dict))
+                                print('index : ',rc['index_detect'])
+                                print('len labels ', len(view_dict[curr_id]['result'][0]['carpart']['labels']))
+
+                                if view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']] == track_labels[c]:
+                                    print('done 1 reverse ')
+                                    # print('record :',rc['detect'],rc['track'])
+                                    # print('cur label : ',view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']])
+                                    view_dict[curr_id]['result'][0]['carpart']['labels'][rc['index_detect']] = detect_labels[r]
+                                    origin_id = rc['index_track']
+                    
+                    # relabel the root of wrong tracking problem
+                    # try :
+                    print('origin frame id ',view_dict[curr_id-1]['frame_id'])
+                    view_dict[curr_id-1]['result'][0]['carpart']['labels'][origin_id] = detect_labels[r]
+                    # except :
+                    #     print('something wrong')
+                    # reset cache 
+
+                    # self.cache[cache_label]=[self.count_frame,1]
+                    self.cache[cache_label]=[0,0]
+
+                    # self.cache[cache_label][0] = self.count_frame
+                    # self.cache[cache_label].insert(1,1)
+                    continue
+
+                detect_info[r][0] = track_labels[c]
+                pred_json[0]['carpart']['labels'][detect_ind[r]] = track_labels[c]
+
+                record = {'frame_id':frame_id,'index_detect':detect_ind[r],'detect':detect_labels[r],'index_track':track_ind[c],'track':track_labels[c]}
+
+                if view_id not in self.history.keys():
+                    self.history[view_id] = [record]
+                else:
+                    self.history[view_id].append(record)
+
+                with open('video_tool/history-data-'+file_name+'.json', 'w', encoding='utf-8') as f:
+                    json.dump(self.history, f, ensure_ascii=False, indent=4)
+
+                check_relabel_flag = True
+
+                print('relabel : ',detect_labels[r], track_labels[c],cost_global[r][c])
+
+                # print('debug pred_json : ',pred_json[0]['carpart']['labels'][detect_ind[r]])
+
+        return detect_info, pred_json, check_relabel_flag, view_dict
+
     def matching_points(self,points1,points2):
         def normalize_point(p):
             return torch.tensor([p[0]/self.W,p[1]/self.H])
@@ -231,6 +387,9 @@ class Hungarian():
         if carpart_label == 'rbu_rear_bumper+b' and (view < 90 or view > 270):
             return False
 
+        if carpart_label == 'tail_gate+b' and (view < 135 or view > 225):
+            return False
+
         return True
 
     def matching_damages(self,carpart_label,boxes1,ind_list1,coord_list1,info1,boxes2,ind_list2,coord_list2,info2):
@@ -255,24 +414,32 @@ class Hungarian():
         
         row_ind,col_ind = linear_sum_assignment(cost_global)
         
-        debug_label = 'rbu_rear_bumper+b'
+        debug_label = 'hood+f'
 
-        # if carpart_label == debug_label:
-        #     print(cost_global)
+        print('check views pair : ',info1['frame_id'],info2['frame_id'])
 
+        if carpart_label == debug_label:
+            print(cost_global)
+        
         for r,c in zip(row_ind,col_ind):
             if r >= len(boxes1) or c >= len(boxes2):
                 continue
             # print('coord : ',coord_list1[r] , coord_list2[c])
-            if cost_global[r,c] < 0.13 and abs(coord_list1[r] - coord_list2[c]) <= 1 \
-            and (self.check_wrong_carpart_view(carpart_label,info1['view']) and self.check_wrong_carpart_view(carpart_label,info2['view'])):
-                print('pair : ',r,c)
-                info1['damage_result'][carpart_label][ind_list1[r]][4] = True
-                info2['damage_result'][carpart_label][ind_list2[c]][4] = True
+            if cost_global[r,c] < 0.13 and \
+            (self.check_wrong_carpart_view(carpart_label,info1['view']) and self.check_wrong_carpart_view(carpart_label,info2['view'])):
+                print('score coord : ',abs(coord_list1[r] - coord_list2[c]))
+                if abs(coord_list1[r] - coord_list2[c]) <= 1 : 
+                    print('pair : ',r,c)
+                    info1['damage_result'][carpart_label][ind_list1[r]][4] = True
+                    info2['damage_result'][carpart_label][ind_list2[c]][4] = True
+                else: 
+                    print('relfection pair : ',r,c)
+                    info1['damage_result'][carpart_label][ind_list1[r]][4] = False
+                    info2['damage_result'][carpart_label][ind_list2[c]][4] = False
 
-        if carpart_label == debug_label:
-            print(info1['view'])
-            print(info2['view'])
+        # if carpart_label == debug_label:
+        #     print(info1['view'])
+        #     print(info2['view'])
 
         return info1, info2
 
---
+++ video_tool/video_new_pipeline.py
@@ -0,0 +1,300 @@
+from curses import flash
+import enum
+from unittest import result
+from video_process import yolo_models
+from mmdet.apis import init_detector, inference_detector, show_result_pyplot
+from ssod.apis.inference import init_detector as st_init_detector
+
+import numpy as np
+import cv2
+import os
+
+from video_process.Hungarian_matching import Hungarian
+from video_process.matching import estimate_position
+from video_process.carpart.segment import CarpartInfo
+# from video_process.damage import scratch
+
+cp_model = init_detector('checkpoint/carpart_rear_exp_2/carpart_rear.py',
+                      'checkpoint/carpart_rear_exp_2/epoch_29.pth',device='cuda:0')
+
+car_model = init_detector('thirdparty/mmdetection/configs/cbnet/mask_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.py',
+                          'checkpoint/coco_pretrain/mask_rcnn_cbv2_swin_tiny_patch4_window7_mstrain_480-800_adamw_3x_coco.pth',device='cuda:0')
+
+models = {'car':car_model,'carpart':cp_model}
+
+carpart_filter = CarpartInfo()
+
+def get_car_carpart_model_prediction(pred_json,image):
+    pred_json[0]['car'] = car_model_inference(models['car'],image)
+    pred_json[0]['carpart'] = carpart_model_inference(models['carpart'],image,0.5)
+    
+    return pred_json
+
+def get_car_carpart_model_prediction(image):
+    out = {}
+    out['car'] = car_model_inference(models['car'],image)
+    out['carpart'] = carpart_model_inference(models['carpart'],image,0.5)
+    
+    return out
+
+def carpart_model_inference(model, image,score_thre=0.5):
+    result = inference_detector(model,image)
+    img_,pred_boxes,pred_segms,pred_labels,pred_scores = show_result_pyplot(model, image.copy(), result,score_thr=0.1)
+    
+    if len(pred_boxes) == 0:
+        return {'labels':[],'scores':[],'masks':[],'bboxes':[]}
+    
+    out_pred = []
+    for pred in pred_boxes:
+        box = [p for b in pred for p in b]
+        out_pred.append(box)
+    
+    index = []
+
+    for idx,label in enumerate(pred_labels):
+        if any([i in model.CLASSES[label] for i in ['door','fender','quarter_panel']]):
+            if pred_scores[idx] > 0.35:
+                index.append(True)
+            else:
+                index.append(False)
+        elif any([i in model.CLASSES[label] for i in ['front_bumper','rear_bumper']]) :
+            if pred_scores[idx] > 0.5:
+                index.append(True)
+            else:
+                index.append(False)
+        else:
+            if pred_scores[idx] > score_thre:
+                index.append(True)
+            else:
+                index.append(False)
+    
+    pred_labels = pred_labels[index]
+    out_pred = np.array(out_pred)[index].tolist()
+    pred_scores = pred_scores[index]
+    pred_segms = pred_segms[index]
+
+    return {"labels": [model.CLASSES[l] for l in pred_labels], "scores": pred_scores.tolist(), "masks": [s.astype(np.uint8) for s in pred_segms],'bboxes':out_pred}
+
+def car_model_inference(model, image):
+    ## car inference 
+    result = inference_detector(model,image)
+    img_,pred_boxes,pred_segms,pred_labels,pred_scores = show_result_pyplot(model, image.copy(), result,score_thr=0.5)
+    
+    index = []
+    for l in pred_labels:
+        if model.CLASSES[l] in ['car','truck']:
+            index.append(True)
+        else:
+            index.append(False)
+    
+    pred_labels = pred_labels[index]
+    pred_boxes = np.array(pred_boxes)[index].tolist()
+    pred_scores = pred_scores[index]
+    pred_segms = pred_segms[index]
+
+    out_pred = []
+    for pred in pred_boxes:
+        box = [p for b in pred for p in b]
+        out_pred.append(box)
+
+    return {"labels": [model.CLASSES[l] for l in pred_labels], "scores": pred_scores.tolist(), "masks": [s.astype(np.uint8) for s in pred_segms],'bboxes':out_pred}
+
+def create_pseudo_mask(image,bbox):
+    mask = np.zeros_like(image[:,:,0]).astype(np.uint8)
+
+    x1,y1,x2,y2=bbox
+    center_x=int((x1+x2)/2)
+    center_y=int((y1+y2)/2)
+    radius= max(10,int(0.1*(min(x2-x1,y2-y1))))
+    
+    mask = cv2.circle(mask, (center_x,center_y), radius, (1), -1)
+    
+    return mask
+
+def yolo_damage_inference(image):
+    pred_json = {}
+    flag = False
+    
+    scratch_boxes, scratch_confs,_ = yolo_models['scratch'](image)
+    scratch_masks = [create_pseudo_mask(image,box) for box in scratch_boxes]
+    scratch_labels = ['scratch' for b in scratch_boxes]
+
+    pred_json['scratch'] = {'labels':scratch_labels,'scores':scratch_confs,'masks':scratch_masks,'bboxes':scratch_boxes}
+
+    dent_boxes, dent_confs,_ = yolo_models['dent'](image)
+    dent_masks = [create_pseudo_mask(image,box) for box in dent_boxes]
+    dent_labels = ['dent' for b in dent_boxes]
+
+    pred_json['dent'] = {'labels':dent_labels,'scores':dent_confs,'masks':dent_masks,'bboxes':dent_boxes}
+
+    if len(scratch_boxes) > 0 or len(dent_boxes) > 0:
+        flag = True
+
+    return [pred_json],flag
+
+class FrameQueue():
+    def __init__(self):
+        self.queue = []
+        self.frame_id_list = []
+        self.length = 0
+        self.max_length = 10
+    
+    def enqueue(self,frame_info):
+        if frame_info['frame_id'] in self.frame_id_list:
+            return
+
+        if self.length < self.max_length :
+            self.queue.append(frame_info)
+            self.frame_id_list = [i['frame_id'] for i in self.queue]
+            self.length += 1
+
+            return
+        
+        self.queue.pop(0)
+        self.queue.append(frame_info)
+        self.frame_id_list = [i['frame_id'] for i in self.queue]
+    
+    def get_queue(self):
+        return self.queue
+
+def draw_result(pred_json,image):
+    pred_json = pred_json[0]
+
+    for cate in pred_json.keys():
+        # print(cate)
+        if cate not in ['car','carpart']:
+            for idx,box in enumerate(pred_json[cate]['bboxes']):
+                image = cv2.rectangle(image,(box[0],box[1]),(box[2],box[3]),(255,0,0),2)
+                cv2.putText(image,pred_json[cate]['labels'][idx],(box[0],box[3]),cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.9,color=(0,255,0),thickness=2)
+
+        if cate == 'carpart':
+            for idx,b in enumerate(pred_json[cate]['bboxes']):
+                label_carpart = get_label(pred_json[cate]['labels'][idx])
+                if any([i in label_carpart for i in ['door','fender','quarter_panel','tail_light','head_light','front_bumper','rear_bumper','mirror']]):
+                    #draw detection info
+                    cv2.rectangle(image,(b[0],b[1]),(b[2],b[3]),(0,0,255),2)
+                    # score = round(pred_json[cate]['scores'][idx],2)
+                    cv2.putText(image,pred_json[cate]['labels'][idx],(b[0],b[3]),cv2.FONT_HERSHEY_SIMPLEX,fontScale=0.9,color=(0,0,255),thickness=2)
+
+    return image 
+
+def get_label(label):
+    return label[:label.find('+')]
+
+def get_car_info(pred_json,car_info_dict_by_frame,frame_queue,hungarian,file_name):
+    hungarian.reset_count_frame() 
+    tracking_flag = False
+    roi_list = []
+    view_dict_by_id = {}
+    view_id = 0
+
+    track_label_list = ['door','fender','quarter_panel','tail_light','head_light','front_bumper','rear_bumper','mirror']
+
+    for item in frame_queue.get_queue():
+        frame = item['frame']
+        frame_id = item['frame_id']
+
+        tracking_info = []
+        if tracking_flag : 
+            for roi_data in roi_list:
+                dst = estimate_position(pre_frame,frame,roi_data[1])
+
+                if dst is None:
+                    continue
+                
+                roi_estimate_info = [roi_data[0],np.array(dst).reshape(-1,2).tolist(),roi_data[2]]
+                tracking_info.append(roi_estimate_info)
+            
+            tracking_flag = False
+        
+        if not tracking_flag : 
+            if frame_id in car_info_dict_by_frame.keys():
+                car_info = car_info_dict_by_frame[frame_id]
+            else: 
+                print('do inference car !!')
+                car_info = get_car_carpart_model_prediction(frame.copy())
+                car_info = carpart_filter.add_carpart_info(['/krug'],[car_info])
+                # car_info_dict[frame_id] = car_info
+            
+            roi_list = []
+
+            for idx,b in enumerate(car_info[0]['carpart']['bboxes']):
+                label_carpart = get_label(car_info[0]['carpart']['labels'][idx])
+                if any([i in label_carpart for i in track_label_list]):
+                    roi = [[int(b[0]),int(b[1])],[int(b[2]),int(b[1])],[int(b[2]),int(b[3])],[int(b[0]),int(b[3])]]
+                    roi_list.append([car_info[0]['carpart']['labels'][idx],roi,idx])
+                    tracking_flag = True
+
+            roi_list, car_info, _,view_dict_by_id = hungarian.bipartite_matching_v2(tracking_info,roi_list,car_info,view_dict_by_id,view_id,frame_id,file_name)
+            # car_info_dict[frame_id] = car_info
+
+            view_info = {'frame_id':frame_id,'frame':frame,'result':car_info}
+            view_dict_by_id[view_id] = view_info
+            view_id += 1
+        # roi_list = []
+        
+
+        # for idx, box in enumerate(car_info['carpart']):
+
+        pre_frame = frame.copy()
+
+
+    #update car_info_dict_by_frame
+    for view_id, info in view_dict_by_id.items():
+        # if info['frame_id'] not in car_info_dict_by_frame.keys():
+        car_info_dict_by_frame[info['frame_id']] = info['result']
+
+    pred_json[0]['car'] = car_info[0]['car']
+    pred_json[0]['carpart'] = car_info[0]['carpart']
+    
+    return pred_json
+
+def evaluate_video(video_path):
+    name = os.path.basename(video_path)
+    name = name[:name.rfind('.')]
+
+    cap = cv2.VideoCapture(video_path)  
+    frame_w = int(cap.get(3))
+    frame_h = int(cap.get(4))
+
+    hungarian = Hungarian(frame_w,frame_h)
+
+    frame_id = 0
+    frame_queue = FrameQueue()
+    car_info_dict_by_frame = {}
+
+    skip = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret != True:
+            break
+        
+        if frame_id % 2 == 0:
+            result, car_info_flag = yolo_damage_inference(frame)
+
+            if car_info_flag : 
+                print('frame id !!!!!!!!!!!!: ',frame_id)
+                frame_queue.enqueue({'frame':frame,'frame_id':frame_id})
+                # run inference on framequeue
+                # ...
+                result = get_car_info(result, car_info_dict_by_frame,frame_queue,hungarian,name)
+                print('debug car info dict: ',len(car_info_dict_by_frame.keys()),' len frame queue : ',len(frame_queue.frame_id_list))
+
+            if frame_id % 4 == 0 : 
+                frame_queue.enqueue({'frame':frame,'frame_id':frame_id})
+            
+            cv2.imwrite('video_tool/debug_new_pipeline.jpg',draw_result(result,frame.copy()))
+
+            # pre_frame = frame.copy()
+        frame_id += 1
+    
+
+    return 
+
+def main():
+    evaluate_video('video_tool/demo.avi') 
+
+if __name__=='__main__':
+    main()
